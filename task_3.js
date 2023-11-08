381499caf123ebf3c4d4e34c2a618623a9e3efb3
Serjikdn <serjik.dn@gmail.com>
2023-11-08 19:32:47 +0200

    toCamelCase complete

--- task_3.js
+++ task_3.js
@@ -6,6 +6,12 @@ let text = 'Hi bro. 2012-11-11 and 2000-01-13';
 console.log(findDate(text));
 
 
-function toCamelCase(text){
-
+function toCamelCase(text) {
+    let words = text.match(/([a-z1-9]+)/g);
+    console.log(words);
+    return words
+        .map((word, index) => index !== 0 ? word.charAt(0).toUpperCase() + word.slice(1) : word)
+        .join("");
 }
+
+console.log(toCamelCase('new_brand_product_'));
